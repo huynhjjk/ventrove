1b30be736135c13f8847a6485115c2e1d28ae120
Johnson <johnson@Johnsons-MacBook-Pro.local>
2017-12-09 09:29:53 -0800

    Adding trends api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var request = require('request');
 var Q = require('q');
+var cheerio = require('cheerio');
 var express = require('express');
+
 
 var app = express();
 
@@ -11,4 +14,40 @@ var server = app.listen(3000, 'localhost', function () {
 	var host = server.address().address
 	var port = server.address().port
 	console.log('Listening at http://%s:%s', host, port)
+  // getSubReddit('litecoin').then(function(urls) {});
+  getGoogleTrends().then(function(urls) {});
 });
+
+var getSubReddit = function(searchQuery) {
+  var deferred = Q.defer();
+  var url = 'https://www.reddit.com/r/' + searchQuery;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var urls = [];
+    $('a.title').each(function() {
+      console.log($(this).first().text());
+      // urls.push($(this).children().first().attr('href'));
+    });
+    deferred.resolve(urls);
+    }
+  });
+  return deferred.promise;
+}
+
+var getGoogleTrends = function() {
+  var deferred = Q.defer();
+  var url = 'https://trends.google.com/trends/hottrends/atom/hourly';
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var urls = [];
+      $('a').each(function() {
+        console.log($(this).first().text());
+        // urls.push($(this).children().first().attr('href'));
+      });
+    deferred.resolve(urls);
+    }
+  });
+  return deferred.promise;
+}
