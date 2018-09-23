5e154ee3dfe252c9c918118944f1deb2498e3844
Johnson Huynh <huynhjjk@gmail.com>
2018-09-23 09:15:16 -0700

    Cleaning up nodemodules

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const util = require('util')
-var request = require('request');
-var async = require('async');
-var Q = require('q');
-var cheerio = require('cheerio');
-var express = require('express');
-var sentiment = require('sentiment');
-var googleTrends = require('google-trends-api');
-var googleFinance = require('google-finance');
+const request = require('request');
+const async = require('async');
+const Q = require('q');
+const cheerio = require('cheerio');
+const express = require('express');
+const sentiment = require('sentiment');
+const googleTrends = require('google-trends-api');
+const googleFinance = require('google-finance');
+const chalk = require('chalk');
 
-var app = express();
+const app = express();
 
 app.get('/', function (req, res) {
-	res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-var server = app.listen(3000, 'localhost', function () {
-	var host = server.address().address
-	var port = server.address().port
-	console.log('Listening at http://%s:%s', host, port)
+const server = app.listen(3000, 'localhost', function () {
+	const host = server.address().address
+	const port = server.address().port
+  console.log('Listening at http://%s:%s', host, port)
+  console.log(chalk.blue('Hello world!'));
   // googleFinance.companyNews({
   //   symbol: 'NASDAQ:AAPL'
   // }, function (err, news) {
@@ -28,13 +30,13 @@ var server = app.listen(3000, 'localhost', function () {
   // getGoogleTrendsKeywords().then(function(keywords) {
   //   console.log(keywords);
   // });
-  // var text = "The movie attempts to be surreal by incorporating various time paradoxes,"+
+  // const text = "The movie attempts to be surreal by incorporating constious time paradoxes,"+
   //               "but it's presented in such a ridiculous way it's seriously boring.";
-  // var r1 = sentiment(text);
+  // const r1 = sentiment(text);
   // console.log(r1);
   // googleTrends.interestOverTime({keyword: 'bitcoin', startTime: new Date('2017-01-01'), endTime: new Date(Date.now())})
   // .then(function(results){
-  //   var timelineData = JSON.parse(results).default.timelineData;
+  //   const timelineData = JSON.parse(results).default.timelineData;
   //   console.log(timelineData);
   // })
   // .catch(function(err){
@@ -44,10 +46,10 @@ var server = app.listen(3000, 'localhost', function () {
 
 	// googleTrends.relatedTopics({keyword: 'bitcoin', startTime: new Date('2017-01-01'), endTime: new Date(Date.now())})
 	// .then((results) => {
-	// 	var relatedKeyWords = [];
-	//  var relatedTopics = JSON.parse(results).default.rankedList;
-	//  for (var i = 0; i < relatedTopics.length; i++) {
-	// 	 for (var j = 0; j < relatedTopics[i].rankedKeyword.length; j++) {
+	// 	const relatedKeyWords = [];
+	//  const relatedTopics = JSON.parse(results).default.rankedList;
+	//  for (const i = 0; i < relatedTopics.length; i++) {
+	// 	 for (const j = 0; j < relatedTopics[i].rankedKeyword.length; j++) {
 	// 		 console.log(util.inspect(relatedTopics[i]['rankedKeyword'][j]['topic']['title'], false, null));
 	// 	 }
 	//  };
@@ -57,13 +59,13 @@ var server = app.listen(3000, 'localhost', function () {
 	// })
 });
 
-var getSubReddit = function(searchQuery) {
-  var deferred = Q.defer();
-  var url = 'https://www.reddit.com/r/' + searchQuery;
+const getSubReddit = function(searchQuery) {
+  const deferred = Q.defer();
+  const url = 'https://www.reddit.com/r/' + searchQuery;
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(body);
-      var urls = [];
+      const $ = cheerio.load(body);
+      const urls = [];
     $('a.title').each(function() {
       console.log($(this).first().text())
       // urls.push($(this).children().first().attr('href'));
@@ -74,15 +76,15 @@ var getSubReddit = function(searchQuery) {
   return deferred.promise;
 }
 
-var getGoogleTrendsKeywords = function() {
-  var deferred = Q.defer();
-  var url = 'https://trends.google.com/trends/hottrends/atom/hourly';
+const getGoogleTrendsKeywords = function() {
+  const deferred = Q.defer();
+  const url = 'https://trends.google.com/trends/hottrends/atom/hourly';
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(body);
-      var keywords = [];
+      const $ = cheerio.load(body);
+      const keywords = [];
       $('a').each(function(index) {
-        var keyword = {
+        const keyword = {
             rank: index + 1,
             title: $(this).first().text(),
             sentiment: sentiment($(this).first().text())
@@ -95,9 +97,9 @@ var getGoogleTrendsKeywords = function() {
   return deferred.promise;
 }
 
-var getFinalStocks = function(multiSymbolsArray){
-  var stockPromises = [];
-  for (var i = 0; i < multiSymbolsArray.length; i++) {
+const getFinalStocks = function(multiSymbolsArray){
+  const stockPromises = [];
+  for (const i = 0; i < multiSymbolsArray.length; i++) {
     stockPromises.push(googleYahooRequests(multiSymbolsArray[i]));
   }
   return Q.all(stockPromises);
